5c8d76a478e7528e416850c9fc424de783ed9c29
NtemKenyor <nkenyor@gmail.com>
2024-10-24 04:56:02 +0000

    on it

--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -16,6 +16,8 @@ def get_weth():
         config["wallets"]["from_key"]
     )  # add your keystore ID as an argument to this call
     weth = interface.WethInterface(config["networks"][network.show_active()]["weth"])
-    tx = weth.deposit({"from": acct, "value": 1000000000000000000})
+    # tx = weth.deposit({"from": acct, "value": 1000000000000000000})
+    # reducing the vaue that is sent to for the conteact...
+    tx = weth.deposit({"from": acct, "value": 1000000000000000})
     print("Received 1 WETH")
     return tx
